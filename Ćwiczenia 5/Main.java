4d9ca56ecb48dc1ec503465c583a04f3a894f446
ChudawskiD <s999704@pjwstk.edu.pl>
2025-05-30 19:04:38 +0200

    Add files via upload

--- Ćwiczenia 5/Main.java
+++ Ćwiczenia 5/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = {
-                new Cat(),
-                new Dog(),
-                new Tiger(),
-                new Wolf()
-        };
-
-        for (Animal a : animals) {
-            a.makeNoise();
-            a.roam();
-            System.out.println();
-        }
+        Car car = new Car(5);
+        car.start();
+        System.out.println("Liczba miejsc: " + car.getNumberOfSeats());
+        car.stop();
     }
 }

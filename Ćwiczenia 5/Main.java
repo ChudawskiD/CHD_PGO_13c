8b42cc007f43dcf13a82f634e0904c055ce2dfc9
ChudawskiD <s999704@pjwstk.edu.pl>
2025-05-30 19:05:50 +0200

    Delete Ćwiczenia 5 directory

--- Ćwiczenia 5/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Car car = new Car(5);
-        car.start();
-        System.out.println("Liczba miejsc: " + car.getNumberOfSeats());
-        car.stop();
-    }
-}
